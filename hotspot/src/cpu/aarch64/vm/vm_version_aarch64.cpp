1ae07a9ddf7e951a0ad52a1b3c2e648809846e8f
usersxx <97012123+usersxx@users.noreply.github.com>
2023-03-11 20:41:16 +0530

    Update vm_version_aarch64.cpp

--- hotspot/src/cpu/aarch64/vm/vm_version_aarch64.cpp
+++ hotspot/src/cpu/aarch64/vm/vm_version_aarch64.cpp
@@ -33,9 +33,6 @@
 #ifdef TARGET_OS_FAMILY_linux
 # include "os_linux.inline.hpp"
 #endif
-#ifdef TARGET_OS_FAMILY_linux
-# include "os_linux.inline.hpp"
-#endif
 #ifdef TARGET_OS_FAMILY_bsd
 # include "os_bsd.inline.hpp"
 #endif
