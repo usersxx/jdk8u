a3805e733fd9097d88485b0e5a5e77ab22ef4fd6
usersxx <97012123+usersxx@users.noreply.github.com>
2023-03-13 15:47:18 +0530

    Create assembler_bsd_aarch64.cpp

---
+++ hotspot/agent/src/share/classes/sun/jvm/hotspot/debugger/bsd/aarch64/assembler_bsd_aarch64.cpp
@@ -0,0 +1,23 @@
+/*
+*
+*/
+
+#include "precompiled.hpp"
+#include "asm/macroAssembler.hpp"
+#include "asm/macroAssembler.inline.hpp"
+#include "runtime/os.hpp"
+#include "runtime/threadLocalStorage.hpp"
+
+void MacroAssembler::get_thread(Register dst) {
+  RegSet saved_regs = RegSet::range(r0, r17) + RegSet::range(r19, r20) + lr - dst;
+  push(saved_regs, sp);
+  mov(c_rarg0, ThreadLocalStorage::thread_index());
+  mov(r19, CAST_FROM_FN_PTR(address, pthread_getspecific));
+  blr(r19);
+  if (dst != c_rarg0) {
+    mov(dst, c_rarg0);
+  }
+  // restore pushed registers
+  pop(saved_regs, sp);
+}
+
